338506a980696007a44f78ea0f329cfedcf92d28
lindedekker <l.dekker-11@prinsesmaximacentrum.nl>
2024-03-26 17:34:12 +0100

    Changed the functions; now they are working

--- R/Sample_Size_Calculation_Copy.R
+++ R/Sample_Size_Calculation_Copy.R
@@ -2,12 +2,34 @@
 library(powerSurvEpi)
 
 # functions for power- and sample size calculation for non-binary covariates.
-powerEpiCont.default()
 ssizeEpiCont.default()
+powerEpiCont.default()
 
 # load database
 setwd("C:/Users/ldekke11/OneDrive - Prinses Maxima Centrum/PhD Project/Courses/Writing_Reproducible_Code/testproject/data/raw")
 cohort <- read.csv("cohort_for_course_WritingReproducibleCode.csv", header = TRUE)
+
+### Sample Size Calculation###
+
+# create function for sample size calculation for continuous predictor
+samplesize_function <- function(x) {
+  sample_size <- ssizeEpiCont.default(
+    power = 0.806, # postulated power
+    theta = theta,
+    sigma2 = var(predictor), # variance of the covariate of interest
+    psi = mean(cohort[[x]] == 1), # proportion of subjects with an event
+    rho2 = sqrt(cor(predictor, cohort[[x]])),
+    alpha = 0.05 # type I error rate
+  )
+  print(c(x, sample_size))
+}
+
+# change input
+predictor <- cohort$AUC_Clo # the covariate of interest
+theta <- 4.8 # postulated hazard ratio for the covariate of interest
+
+# calculate sample size for each outcome variable
+sample_sizes <- lapply(colnames(cohort)[3:11], samplesize_function)
 
 ### Power Calculation ###
 
@@ -16,76 +38,18 @@ power_function <- function(x) {
   power <- powerEpiCont.default(
     n = n,
     theta = thetha,
-    sigma2 = sigma2,
-    psi = psi,
-    rho2 = rho2,
+    sigma2 = var(predictor), # variance of the covariate of interest
+    psi = mean(cohort[[x]] == 1), # numeric proportion of subjects with an event
+    rho2 = sqrt(cor(predictor, cohort[[x]])),
     alpha = 0.05
   )
   print(c(x, power))
 }
 
 # change input
-n <- 103
-theta <- 2.0 # numeric postulated hazard ratio
-sigma2 <- var() # numeric variance of the covariate of interest
-psi <- # numeric proportion of subjects died of the disease of interest
-  rho2 <-
-  ### Sample Size Calculation###
+predictor <- cohort$AUC_Clo # the covariate of interest
+n <- 103 # number of subjects included
+theta <- 2.0 # postulated hazard ratio for the covariate of interest
 
-  # create function for sample size calculation for continuous predictor
-  samplesize_function <- function(x) {
-    sample_size <- ssizeEpiCont.default(
-      power = 0.806, # postulated power
-      theta = theta,
-      sigma2 = sigma2,
-      psi = mean(cohort[[x]] == 1), # proportion of subjects with an event
-      rho2 = sqrt(cor(predictor, cohort[[x]])),
-      alpha = 0.05 # type I error rate
-    )
-    print(c(x, sample_size$n))
-  }
-
-# change input
-predictor <- cohort$AUC_Clo
-theta <- 4.8 # Hazard ratio (HR) for the continuous predictor
-sigma2 <- var(predictor) # variance of the covariate of interest
-
-sample_sizes <- lapply(colnames(cohort)[3:11], samplesize_function) 
-
-
-### Sample Size Calculation ###
-
-alpha <- 0.05 # Significance level
-power <- 0.806 # Desired power
-theta <- 4.8 # Hazard ratio (HR) for the continuous predictor
-
-# Sample size for continuous covariate FluAUC
-var.X1 <- "FluAUCtotal"
-sample_sizes <- lapply(colnames(cohort)[3:10], function(x) {
-  sample_size <- ssizeEpiCont(
-    formula = cohort$FluAUCtotal ~ cohort[[x]],
-    dat = cohort,
-    var.X1,
-    var.failureFlag = x,
-    power,
-    theta,
-    alpha = alpha
-  )
-  print(c(x, sample_size$n))
-})
-
-
-# Sample size for continuous covariate CloAUC
-var.X1 <- "AUC_Clo"
-sample_sizes <- lapply(colnames(cohort)[3:10], function(x) {
-  sample_size <- ssizeEpiCont(
-    formula = cohort$AUC_Clo ~ cohort[[x]],
-    dat = cohort,
-    var.X1,
-    var.failureFlag = x,
-    power,
-    theta,
-    alpha = alpha
-  )
-  print(c(x, sample_size$n))
-})
+#calculate power for each outcome variable
+power_calculation <- lapply(colnames(cohort)[3:11], power_function)
